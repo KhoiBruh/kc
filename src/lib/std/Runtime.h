4bd86733574277f6d244ff0e0e614a3c132e52f9
KhoiBell <diotekd0@gmail.com>
2026-07-27 11:25:46 +0700

    Implement standard library components including memory allocation, collections, and core modules

--- src/lib/std/Runtime.h
+++ src/lib/std/Runtime.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
 
+extern "C" void* k_std_alloc(std::uint64_t size);
+extern "C" void k_std_free(void* pointer);
 extern "C" void k_std_print_bytes(const char* bytes, std::uint64_t length);
 extern "C" void k_std_print_i32(std::int32_t value);
